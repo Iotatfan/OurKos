69b9d10b56dd9ba8980e055dc5870ba76ac4af4a
Muhammad Iqbal Imani Atfan <42773954+Iotatfan@users.noreply.github.com>
2019-10-07 18:58:04 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ourkos/data/LoginDataSource.java
@@ -0,0 +1,29 @@
+package com.example.ourkos.data;
+
+import com.example.ourkos.data.model.LoggedInUser;
+
+import java.io.IOException;
+
+/**
+ * Class that handles authentication w/ login credentials and retrieves user information.
+ */
+public class LoginDataSource {
+
+    public Result<LoggedInUser> login(String username, String password) {
+
+        try {
+            // TODO: handle loggedInUser authentication
+            LoggedInUser fakeUser =
+                    new LoggedInUser(
+                            java.util.UUID.randomUUID().toString(),
+                            "Jane Doe");
+            return new Result.Success<>(fakeUser);
+        } catch (Exception e) {
+            return new Result.Error(new IOException("Error logging in", e));
+        }
+    }
+
+    public void logout() {
+        // TODO: revoke authentication
+    }
+}
