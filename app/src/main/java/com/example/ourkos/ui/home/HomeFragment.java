beef176a5ecccc9e61f85733a2a7ad72fa8e25d4
Muhammad Iqbal Imani Atfan <42773954+Iotatfan@users.noreply.github.com>
2019-10-29 23:42:29 +0700

    From Tester
    Search View Now Working

--- app/src/main/java/com/example/ourkos/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/ourkos/ui/home/HomeFragment.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -14,16 +17,68 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.ourkos.R;
 
-public class HomeFragment extends Fragment {
+import java.util.ArrayList;
 
+public class HomeFragment extends Fragment {
     private HomeViewModel homeViewModel;
+    ListView listView;
+    SearchView searchView;
+    ArrayList<String> list;
+    ArrayAdapter adapter;
+
+    View root;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-//        final TextView textView = root.findViewById(R.id.textView);
+        root = inflater.inflate(R.layout.fragment_home, container, false);
+        searchView = root.findViewById(R.id.cari);
+//        searchView.setIconified(true);
+        searchView.setSubmitButtonEnabled(false);
+        listView = root.findViewById(R.id.list);
+
+        listView.setVisibility(root.GONE);
+
+        list = new ArrayList<>();
+        list.add("Semarang");
+        list.add("Jakarta");
+        list.add("Bandung");
+        list.add("Surabaya");
+        list.add("Solo");
+        list.add("Yogyakarta");
+        list.add("Malang");
+        adapter = new ArrayAdapter<>(getActivity().getApplicationContext(),android.R.layout.simple_list_item_1,list);
+        listView.setAdapter(adapter);
+        searchView.setOnQueryTextFocusChangeListener(new View.OnFocusChangeListener(){
+            @Override
+            public void onFocusChange(View v, boolean hasFocus){
+                if(!hasFocus){
+                    listView.setVisibility(root.GONE);
+                }
+                else{
+                    listView.setVisibility(root.VISIBLE);
+                }
+            }
+        });
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                if(list.contains(query)){
+                    adapter.getFilter().filter(query);
+//                    searchView.clearFocus();
+                }else{
+                    Toast.makeText(getActivity().getApplicationContext(), "No Match found",Toast.LENGTH_LONG).show();
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
         homeViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
