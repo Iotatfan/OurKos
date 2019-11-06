0d50736d6fb226f6d945e790cf22a60980b33546
Muhammad Iqbal Imani Atfan <42773954+Iotatfan@users.noreply.github.com>
2019-11-06 11:36:35 +0700

    UI Color Change

--- app/src/main/java/com/example/ourkos/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/ourkos/ui/home/HomeFragment.java
@@ -1,17 +1,20 @@
 package com.example.ourkos.ui.home;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.SearchView;
 import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -28,6 +31,20 @@ public class HomeFragment extends Fragment {
     ArrayAdapter adapter;
 
     View root;
+    public static int getHeighesttview(Context context, Adapter adapter){
+        int maxHeight = 0;
+        View view = null;
+        RelativeLayout fakeParent = new RelativeLayout(context);
+        for (int i=0, count=adapter.getCount(); i<count; i++) {
+            view = adapter.getView(i, view, fakeParent);
+            view.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+            int height = view.getMeasuredHeight();
+            if (height > maxHeight) {
+                maxHeight = height;
+            }
+        }
+        return maxHeight;
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -42,15 +59,17 @@ public class HomeFragment extends Fragment {
         listView.setVisibility(root.GONE);
 
         list = new ArrayList<>();
-        list.add("Semarang");
-        list.add("Jakarta");
         list.add("Bandung");
+        list.add("Jakarta");
+        list.add("Malang");
+        list.add("Semarang");
         list.add("Surabaya");
         list.add("Solo");
         list.add("Yogyakarta");
-        list.add("Malang");
         adapter = new ArrayAdapter<>(getActivity().getApplicationContext(),android.R.layout.simple_list_item_1,list);
+        adapter.getCount();
         listView.setAdapter(adapter);
+        listView.getLayoutParams().height=getHeighesttview(getActivity().getApplicationContext(),adapter)*5;
         searchView.setOnQueryTextFocusChangeListener(new View.OnFocusChangeListener(){
             @Override
             public void onFocusChange(View v, boolean hasFocus){
@@ -85,6 +104,7 @@ public class HomeFragment extends Fragment {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 String selected = (String)adapterView.getItemAtPosition(i);
                 searchView.setQuery(selected,true);
+//                Intent intent = new Intent(HomeFragment.this,)
             }
         });
         homeViewModel.getText().observe(this, new Observer<String>() {
